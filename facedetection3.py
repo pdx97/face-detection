ba1beb0e0b961c83b7b069848bfa643e20427e06
pdx97 <prakhhar1997@gmail.com>
2017-06-02 16:47:39 +0530

    Add files via upload

---
+++ facedetection3.py
@@ -0,0 +1,41 @@
+import cv2
+import numpy as np
+
+      
+face_cascade = cv2.CascadeClassifier('D:\\opencv\\sources\\data\\haarcascades\\haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('D:\\opencv\\sources\\data\\haarcascades\\haarcascade_eye.xml')
+cam=cv2.VideoCapture(0)
+
+while (cam.isOpened()):
+    ret,img= cam.read()
+    gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    faces=face_cascade.detectMultiScale(gray,1.3,5)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img, (x,y), (x+w,x+h),(255,0,0),2)
+        roi_gray= gray[y:y+h,x:x+w]
+        roi_color= img[y:y+h,x:x+w]
+        eyes=eye_cascade.detectMultiScale(roi_gray)
+        for (ex,ey,ew,eh) in eyes :
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+    
+    cv2.imshow('img',img)
+    k = cv2.waitKey(1)
+    if k ==27:
+        break   
+            
+            
+
+
+    
+
+
+cam.release()
+cv2.destroyAllWindows()
+
+
+    
+
+
+
+
